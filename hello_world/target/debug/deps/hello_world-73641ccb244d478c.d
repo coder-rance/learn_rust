30cf8b3754f730b777445b517b5a11bf5e2a239b
RanceLotus <13626112170@163.com>
2022-07-01 23:41:16 +0800

    first commit

---
+++ hello_world/target/debug/deps/hello_world-73641ccb244d478c.d
@@ -0,0 +1,5 @@
+e:\Code\learn_rust\hello_world\target\debug\deps\hello_world-73641ccb244d478c.rmeta: src\main.rs
+
+e:\Code\learn_rust\hello_world\target\debug\deps\hello_world-73641ccb244d478c.d: src\main.rs
+
+src\main.rs:
